9cccc9ce88fc10e57930a87fc3e991217d04eae1
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-11-30 17:59:26 +0530

    Like  post Api Inetgration

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -15,6 +15,9 @@ const Post = ({ post }) => {
     const [open, setOpen] = useState(false)
     const { user } = useSelector(store => store.auth)
     const { posts } = useSelector(store => store.post);
+    const [liked, setLiked] = useState(post.likes.includes(user?._id) || false);
+    const [postlike, setPostLike] = useState(post.likes.length);
+    const [comment, setComment] = useState(post.comments);
 
     const dispatch = useDispatch();
     const changeEventHandler = (e) => {
@@ -23,6 +26,31 @@ const Post = ({ post }) => {
             setText(InputText)
         } else {
             setText("")
+        }
+    }
+
+    const likeOrDislikeHandler = async () => {
+        try {
+
+            const action = liked ? "dislike" : "like";
+            const res = await axios.get(`http://localhost:4000/api/v1/post/${post._id}/${action}`, { withCredentials: true })
+            console.log(res.data)
+            if (res.data.success) {
+                const updateLikes = liked ? postlike - 1 : postlike + 1
+                setPostLike(updateLikes);
+                setLiked(!liked);
+
+                //apne post ko update krunga
+
+                const updatePostData = posts.map(p => p._id === post._id ? {
+                    ...p, likes: liked ? p.likes.filter(id => id !== user._id) : [...p.likes, user._id]
+                } : p)
+                dispatch(setPosts(updatePostData));
+                toast.success(res.data.message);
+            }
+
+        } catch (error) {
+            console.log(error)
         }
     }
     const deletePostHandler = async () => {
@@ -54,7 +82,9 @@ const Post = ({ post }) => {
                         <MoreHorizontal className='cursor-pointer' />
                     </DialogTrigger>
                     <DialogContent className="flex flex-col items-center text-sm text-center">
-                        <Button variant="ghost" className="cursor-pointer w-fit text-[#ED4956]">Unfollow</Button>
+                        {
+                            post?.author?._id !== user?._id && <Button variant='ghost' className="cursor-pointer w-fit text-[#ED4956] font-bold">Unfollow</Button>
+                        }
                         <Button variant="ghost" className="cursor-pointer w-fit">Add to favorites</Button>
                         {
                             user && user?._id === post?.author._id && <Button onClick={deletePostHandler} variant="ghost" className="cursor-pointer w-fit"> Delete</Button>
@@ -66,13 +96,15 @@ const Post = ({ post }) => {
             <img className='rounded-sm my-2 w-full aspect-square object-cover' src={post.image} alt="post_img" />
             <div className="flex items-center justify-between my-2">
                 <div className="flex item-center gap-3">
-                    <FaRegHeart size={"22px"} className='cursor-pointer hover:text-gray-600' />
+                    {
+                        liked ? <FaHeart onClick={likeOrDislikeHandler} size={'24'} className='cursor-pointer text-red-600' /> : <FaRegHeart onClick={likeOrDislikeHandler} size={'22px'} className='cursor-pointer hover:text-gray-600' />
+                    }
                     <MessageCircle onClick={() => setOpen(true)} className='cursor-pointer hover:text-gray-600' />
                     <Send className='cursor-pointer hover:text-gray-600' />
                 </div>
                 <Bookmark className='cursor-pointer hover:text-gray-600' />
             </div>
-            <span className="font-medium block mb-2">{post.likes.length}likes</span>
+            <span className="font-medium block mb-2">{postlike} likes</span>
             <p>
                 <span className='font-medium mr-2'>{post.author?.username}</span>
                 {post.caption}
