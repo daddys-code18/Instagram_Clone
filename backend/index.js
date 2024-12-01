9765fc285aa28dad3a31d1ccbeee8e063da8fdce
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-12-01 20:58:03 +0530

    socket.io Implementation

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -6,9 +6,10 @@ import connectDb from "./utlis/db.js";
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
+import { app, server } from "./socket/socket.js";
+import path from "path";
 dotenv.config({});
 
-const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Connecting Database
@@ -16,7 +17,7 @@ connectDb();
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
@@ -37,7 +38,7 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   // connectDb();
   console.log(`Server is Listening at Port:${PORT}`);
 });
--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { Button } from './ui/button'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import useGetAllMessage from '@/hooks/useGetAllMessage'
+import useGetAllMessage from '../hooks/useGetAllMessage'
 // import useGetRTM from '@/hooks/useGetRTM'
 
 const Messages = ({ selectedUser }) => {
